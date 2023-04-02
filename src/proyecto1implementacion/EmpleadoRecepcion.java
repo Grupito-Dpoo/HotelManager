403f7266c7e36d74c96e5cac774814f8a95279f7
JohanBautistaQ <j.bautistaq@uniandes.edu.co>
2023-04-02 01:12:05 -0500

    format

--- src/proyecto1implementacion/EmpleadoRecepcion.java
+++ src/proyecto1implementacion/EmpleadoRecepcion.java
@@ -1,25 +1,21 @@
 package proyecto1implementacion;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 public class EmpleadoRecepcion extends UsuarioSistema {
 
 	public EmpleadoRecepcion(String login, String password, Hotel hotel) {
 		super(login, password, hotel);
-
 	}
 
 	public String getLogin() {
 		return super.getLogin();
 	}
-	
+
 	public String getPassword() {
 		return super.getPassword();
 	}
-	
+
 	public Hotel getHotel() {
 		return super.getHotel();
 	}
-	
+
 }
