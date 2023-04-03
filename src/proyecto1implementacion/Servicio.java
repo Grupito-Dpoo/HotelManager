eb09beebcfe03959274e8a5f75fba7624afda3b4
JohanBautistaQ <j.bautistaq@uniandes.edu.co>
2023-04-02 22:30:08 -0500

    servicio

--- src/proyecto1implementacion/Servicio.java
+++ src/proyecto1implementacion/Servicio.java
@@ -6,10 +6,24 @@ import java.io.Serializable;
  *
  * @author Johan Bautista
  */
-public class Servicio implements Serializable{
+public class Servicio implements Serializable {
+
+    private String nombre;
+
+    public Servicio(String nombre) {
+        this.nombre = nombre;
+    }
 
     public enum areaAsociada {
         SPA, RESTAURANTE, GUIATURISTICO
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
 }
